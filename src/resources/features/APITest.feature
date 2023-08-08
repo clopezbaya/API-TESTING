db87b4e6dee8a75796a43d92d694e2ab4f869e13
Christian Lopez <Christian.Lopez@fundacion-jala.org>
2023-08-08 13:54:57 -0400

    First Commit with base for API testing

---
+++ src/resources/features/APITest.feature
@@ -0,0 +1,6 @@
+Feature: Test the first Endpoint
+
+    Api Test
+Scenario: Verigy GET method of API
+Given I send the request
+Then I get list of 10 users
---
+++ src/test/java/Steps/APISteps.java
@@ -0,0 +1,17 @@
+package Steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class APISteps {
+    
+    @Given("I send the request")
+    public void sendRequest(){
+
+    }
+
+    @Then("I get list of {int} users")
+    public void verifyList(int num){
+
+    }
+}
